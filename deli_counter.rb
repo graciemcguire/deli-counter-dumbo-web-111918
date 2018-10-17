fda8c93a788e79b96b14bca6752545f283743e3c
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 19:12:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,7 @@ def line(array)
   counter = 0
   newArray = []
   array.each do |person|
-    newArray.push("The line is currently: #{counter}. #{person}")
+    puts "The line is currently: #{counter}. #{person}"
     counter += 1
   end
-  return newArray
 end
