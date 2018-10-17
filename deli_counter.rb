93ebac3ce12bf209af43cd184cc35bfbbb3f177f
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 18:19:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line(queue)
+  counter = 1
+  while queue.size == 0 
+    puts "The line is currently empty."
+  end  
+  queue.each do |person|
+      puts "The line is currently: #{counter}. #{person}"
+      counter += 1
+  end
+end
