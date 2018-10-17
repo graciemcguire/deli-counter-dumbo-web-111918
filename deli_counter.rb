4a181253ab12259815878677aa502255f6a40f9b
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 19:11:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,9 @@
-def line(queue)
+def line(array)
   counter = 0
-  linelist = []
-  queue.each do |person|
-    linelist.push("The line is currently: #{counter}. #{person}")
+  newArray = []
+  array.each do |person|
+    newArray.push("The line is currently: #{counter}. #{person}")
     counter += 1
-    if counter == 0 
-      puts "The line is currently empty."
-    else
       puts linelist
-    end
   end
 end
